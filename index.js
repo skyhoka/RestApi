4482799742d5581b6afd74e30ffd1b4102edc0d5
skyhoka <setokemailsatu@gmail.com>
2025-01-15 18:30:35 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -374,7 +374,7 @@ app.get("/api/download/alldownloader", async (req, res) => {
         let result = {
         status: true, 
         creator: global.creator, 
-        result: anu.data
+        result: ...anu.data
         }
         res.json(result)
     } catch (error) {
@@ -420,11 +420,10 @@ app.get("/api/download/ytmp3", async (req, res) => {
 
     try {
         var anu = await alldl(`${url}`)
-
         res.json({
             status: true,
             creator: global.creator,
-            result: anu.data
+            result: ...anu.data
         });
     } catch (error) {
         console.log(error);
@@ -442,7 +441,7 @@ app.get("/api/download/ytmp4", async (req, res) => {
         res.json({
             status: true,
             creator: global.creator,
-            result: anu.data
+            result: ...anu.data
         });
     } catch (error) {
         console.log(error);
