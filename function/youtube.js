dd029751d01a9eaae64f83d7320006c15e303c53
skyhoka <setokemailsatu@gmail.com>
2025-01-15 17:53:11 +0700

    Add files via upload

--- function/youtube.js
+++ function/youtube.js
@@ -12,11 +12,11 @@ reject(e)
 })
 }
 
-async function rahadytdl(url) {
+async function YtMp4(url) {
 return new Promise(async (resolve, reject) => {
 const links = url
 try {
-const res = await ytmp4(links)
+const res = await rahadytdl(links)
 resolve(res)
 } catch (e) {
 reject(e) 
