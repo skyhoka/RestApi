6062e2113d506b4195919fcf4aafb28839b1202a
skyhoka <setokemailsatu@gmail.com>
2025-01-15 17:40:41 +0700

    Add files via upload

--- function/youtube.js
+++ function/youtube.js
@@ -1,10 +1,10 @@
-const { ytmp3, ytmp4 } = require('ruhend-scraper')
+const { rahadytdl } = require('rahad-media-downloader')
 
 async function YtMp3(url) {
 return new Promise(async (resolve, reject) => {
 const links = url
 try {
-const res = await ytmp3(links)
+const res = await rahadytdl(links)
 resolve(res)
 } catch (e) {
 reject(e) 
@@ -12,7 +12,7 @@ reject(e)
 })
 }
 
-async function YtMp4(url) {
+async function rahadytdl(url) {
 return new Promise(async (resolve, reject) => {
 const links = url
 try {
